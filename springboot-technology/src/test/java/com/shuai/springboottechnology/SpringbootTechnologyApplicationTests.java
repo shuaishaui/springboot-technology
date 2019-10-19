19ae6cb4a1d83bf5e22bb6cfdbc3152055276988
shuaishaui <1909085704@qq.com>
2019-10-19 18:32:18 +0800

    first commit

---
+++ springboot-technology/src/test/java/com/shuai/springboottechnology/SpringbootTechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuai.springboottechnology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTechnologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
